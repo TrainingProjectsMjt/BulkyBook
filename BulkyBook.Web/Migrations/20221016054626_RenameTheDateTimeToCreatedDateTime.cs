0e7f3ec1a03d314ad9cab4f241cfa34706830ca0
Major Jr Wei Sing Tsai <majortsai@users.noreply.github.com>
2022-10-16 16:22:13 +0800

    Initial commit.

---
+++ BulkyBook.Web/Migrations/20221016054626_RenameTheDateTimeToCreatedDateTime.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBook.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTheDateTimeToCreatedDateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateTime",
+                table: "Categories",
+                newName: "CreatedDateTime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedDateTime",
+                table: "Categories",
+                newName: "DateTime");
+        }
+    }
+}
